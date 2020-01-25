38ffac5105c68931affd98abea21a7562bc7faff
Lauri Miettinen <lauri.miettinen90@gmail.com>
2020-01-25 18:55:15 +0200

    Made detection for allowing pot placement

--- Assets/Scripts/GridTracker.cs
+++ Assets/Scripts/GridTracker.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GridTracker : MonoBehaviour
 {
+    const string waterTileName = "HexTilesetv3_5";
+
     Grid grid;
+    Tilemap tilemap;
     private void Awake() {
         grid = GameObject.Find( "Grid" ).GetComponent<Grid>();
+        tilemap = grid.GetComponentInChildren<Tilemap>();
     }
     void Start() {
         
@@ -16,6 +21,13 @@ public class GridTracker : MonoBehaviour
     void Update() {
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
-        Debug.Log(coordinate);
+
+        bool allowed = AllowedToPlacePot(tilemap.GetSprite(coordinate));
+        Debug.Log( allowed );
+    }
+
+    bool AllowedToPlacePot(Sprite givenSprite) {
+        
+        return givenSprite == null ? false : givenSprite.name == waterTileName;
     }
 }
