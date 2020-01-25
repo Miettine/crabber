504f4463ec892daf65c6ded63f7bc70296661fa0
Lauri Miettinen <lauri.miettinen90@gmail.com>
2020-01-25 19:48:16 +0200

    Working on placement

--- Assets/Scripts/GridTracker.cs
+++ Assets/Scripts/GridTracker.cs
@@ -5,13 +5,22 @@ using UnityEngine.Tilemaps;
 
 public class GridTracker : MonoBehaviour
 {
-    const string waterTileName = "HexTilesetv3_5";
+    const string WaterTileName = "HexTilesetv3_5";
+    const string PlacedPotTileName = "HexTilesetv3_41";
+    const string TerrainGridName = "TerrainGrid";
+    const string MarkerGridName = "MarkerGrid";
 
-    Grid grid;
-    Tilemap tilemap;
+    Grid terrainGrid;
+    Tilemap terrainTilemap;
+
+    Grid markerGrid;
+    Tilemap markerTilemap;
     private void Awake() {
-        grid = GameObject.Find( "Grid" ).GetComponent<Grid>();
-        tilemap = grid.GetComponentInChildren<Tilemap>();
+        terrainGrid = GameObject.Find(TerrainGridName).GetComponent<Grid>();
+        terrainTilemap = terrainGrid.GetComponentInChildren<Tilemap>();
+
+        markerGrid = GameObject.Find(MarkerGridName).GetComponent<Grid>();
+        markerTilemap = markerGrid.GetComponentInChildren<Tilemap>();
     }
     void Start() {
         
@@ -20,14 +29,26 @@ public class GridTracker : MonoBehaviour
     // Update is called once per frame
     void Update() {
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3Int coordinate = grid.WorldToCell(mouseWorldPos);
-
-        bool allowed = AllowedToPlacePot(tilemap.GetSprite(coordinate));
+        
+        bool allowed = AllowedToPlacePot(mouseWorldPos);
         Debug.Log( allowed );
     }
+    bool AllowedToPlacePot(Vector3 worldPos) {
+        Vector3Int terrainCoordinate = terrainGrid.WorldToCell(worldPos);
+        Sprite terrainSprite = terrainTilemap.GetSprite(terrainCoordinate);
 
-    bool AllowedToPlacePot(Sprite givenSprite) {
+        bool allowedToPlaceTerrain = SpriteHasName(terrainSprite, WaterTileName);
+
+
+        Vector3Int markerCoordinate = markerGrid.WorldToCell(worldPos);
+        Sprite markerSprite = terrainTilemap.GetSprite(markerCoordinate);
+
+        bool allowedToPlaceMarker = SpriteHasName(markerSprite, PlacedPotTileName);
+
+        return allowedToPlaceTerrain && allowedToPlaceMarker;
+    }
+     bool SpriteHasName(Sprite givenSprite, string name) {
         
-        return givenSprite == null ? false : givenSprite.name == waterTileName;
+        return givenSprite == null ? false : givenSprite.name == name;
     }
 }
