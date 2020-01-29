097e8ed2543c2236a4ac8f7a4abad0473a82a490
Lauri Miettinen <lauri.miettinen90@gmail.com>
2020-01-29 20:45:47 +0200

    -

---
+++ Assets/Scripts/WaterTile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WaterTile : Tile
+{
+	public int Crab { get; set; }
+	public void DebugGetCrab (){
+		Debug.Log(Crab);
+	}
+}
