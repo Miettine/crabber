2ee5ad8d4edfe76a84afc8d59e2f3d74c1c0ab47
Lauri Miettinen <lauri.miettinen90@gmail.com>
2020-04-24 20:29:52 +0300

    Improved algorithm

--- Assets/Scripts/CubicCrabGrid.cs
+++ Assets/Scripts/CubicCrabGrid.cs
@@ -17,8 +17,9 @@ public class CubicCrabGrid : Dictionary<Vector3Int, int> {
 			AddCrab(amount, location);
 		}
 	}
+
 	/**
-	* There exists algorithms that allow me to draw a ring of hexagons.
+	* There exist algorithms that allow me to draw a ring of hexagons.
 	* However, developing the algorithms would take time and I want to 
 	* get this project over with, so I am making the swarms by hand.
 	*/
@@ -26,35 +27,39 @@ public class CubicCrabGrid : Dictionary<Vector3Int, int> {
 
 		AddCrab(populationConcentration[0], center);
 
-		var p1 = new Vector3Int(center.x, center.y + 1, center.z - 1);
-		var p2 = new Vector3Int(center.x + 1, center.y, center.z - 1);
-		var p3 = new Vector3Int(center.x + 1, center.y - 1, center.z);
-		var p4 = new Vector3Int(center.x, center.y - 1, center.z + 1);
-		var p5 = new Vector3Int(center.x - 1, center.y, center.z + 1);
-		var p6 = new Vector3Int(center.x - 1, center.y + 1, center.z);
+		if (populationConcentration.Length > 1) {
+			var p1 = new Vector3Int(center.x, center.y + 1, center.z - 1);
+			var p2 = new Vector3Int(center.x + 1, center.y, center.z - 1);
+			var p3 = new Vector3Int(center.x + 1, center.y - 1, center.z);
+			var p4 = new Vector3Int(center.x, center.y - 1, center.z + 1);
+			var p5 = new Vector3Int(center.x - 1, center.y, center.z + 1);
+			var p6 = new Vector3Int(center.x - 1, center.y + 1, center.z);
 
-		AddCrab(populationConcentration[1], p1, p2, p3, p4, p5, p6);
+			AddCrab(populationConcentration[1], p1, p2, p3, p4, p5, p6);
+		}
+		
+		if (populationConcentration.Length > 2) {
+			var p7 = new Vector3Int(center.x, center.y + 2, center.z - 2);
 
-		var p7 = new Vector3Int(center.x, center.y + 2, center.z - 2);
+			var p8 = new Vector3Int(center.x + 1, center.y + 1, center.z - 2);
+			var p9 = new Vector3Int(center.x + 2, center.y, center.z -2);
 
-		var p8 = new Vector3Int(center.x + 1, center.y + 1, center.z - 2);
-		var p9 = new Vector3Int(center.x + 2, center.y, center.z -2);
+			var p10 = new Vector3Int(center.x + 2, center.y - 1, center.z - 1);
+			var p11 = new Vector3Int(center.x + 2, center.y - 2, center.z);
 
-		var p10 = new Vector3Int(center.x + 2, center.y - 1, center.z - 1);
-		var p11 = new Vector3Int(center.x + 2, center.y - 2, center.z);
+			var p12 = new Vector3Int(center.x + 1, center.y - 2, center.z + 1);
+			var p13 = new Vector3Int(center.x, center.y - 2, center.z + 2);
 
-		var p12 = new Vector3Int(center.x + 1, center.y - 2, center.z + 1);
-		var p13 = new Vector3Int(center.x, center.y - 2, center.z + 2);
+			var p14 = new Vector3Int(center.x - 1, center.y - 1, center.z + 2);
+			var p15 = new Vector3Int(center.x - 2, center.y, center.z + 2);
 
-		var p14 = new Vector3Int(center.x - 1, center.y - 1, center.z + 2);
-		var p15 = new Vector3Int(center.x - 2, center.y, center.z + 2);
+			var p16 = new Vector3Int(center.x - 2, center.y + 1, center.z + 1);
+			var p17 = new Vector3Int(center.x - 2, center.y + 2, center.z);
 
-		var p16 = new Vector3Int(center.x - 2, center.y + 1, center.z + 1);
-		var p17 = new Vector3Int(center.x - 2, center.y + 2, center.z);
+			var p18 = new Vector3Int(center.x - 1, center.y + 2, center.z -1);
 
-		var p18 = new Vector3Int(center.x - 1, center.y + 2, center.z -1);
-
-		AddCrab(populationConcentration[2], p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18);
+			AddCrab(populationConcentration[2], p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18);
+		}
 	}
 
 	public static Vector3Int OffsetToCubic(Vector3Int offsetPoint){
